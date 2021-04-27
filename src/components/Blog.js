c8a9473a5d36208f010864c651f941f1234d2e9f
Diego Seoane <diegoseoanegonzalez8@gmail.com>
2021-04-27 19:23:33 +0200

    feat: extend blog info

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showAll, setShowAll] = useState(false)
+
+  const changeShowAll = () => {
+    setShowAll(prevState => !prevState)
+  }
+
+  const buttonLabel = showAll ? 'hide' : 'view'
+
+  return (
+    <div>
+      <spam><b>{blog.title}</b> by {blog.author}</spam>
+      <button onClick={changeShowAll}>{buttonLabel}</button>
+      <br />
+      {
+        showAll
+          ? (
+            <>
+              <spam>{blog.url}</spam>
+              <br />
+              <spam>Added by: {blog.user.name}</spam>
+              <div>
+                <spam>Likes: {blog.likes}</spam>
+                <button>like</button>
+              </div>
+            </>
+            )
+          : null
+      }
+    </div>
+  )
+}
 
 export default Blog
