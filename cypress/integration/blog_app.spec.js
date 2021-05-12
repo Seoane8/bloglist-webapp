8a5c781751abd7bbc61f6c174e71eee963a52139
Diego Seoane <diegoseoanegonzalez8@gmail.com>
2021-05-12 16:04:06 +0200

    test: :white_check_mark: add e2e test for the correct functioning of the login form
    Exercise 5.18

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,6 +1,13 @@
+const userInfo = {
+  name: 'John Doe',
+  username: 'JDoe',
+  password: 'pswd'
+}
+
 describe('Blog app', () => {
   beforeEach(() => {
     cy.resetDB()
+    cy.addUser(userInfo)
     cy.visit('http://localhost:3000')
   })
 
@@ -9,5 +16,22 @@ describe('Blog app', () => {
     cy.get('[placeholder="username"]')
     cy.get('[placeholder="password"]')
     cy.contains('Login')
+  })
+
+  describe('Login', () => {
+    it('success with correct credentials', () => {
+      cy.get('[placeholder="username"]').type(userInfo.username)
+      cy.get('[placeholder="password"]').type(userInfo.password)
+      cy.get('form').contains('Login').click()
+      cy.contains(`Logged by ${userInfo.username}`)
+    })
+
+    it('fails with wrong credentials', () => {
+      cy.get('[placeholder="username"]').type('username')
+      cy.get('[placeholder="password"]').type('password')
+      cy.get('form').contains('Login').click()
+      cy.contains('invalid username or password')
+        .should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,3 +3,7 @@ const baseUrl = 'http://localhost:3001/api'
 Cypress.Commands.add('resetDB', () => {
   cy.request('POST', `${baseUrl}/testing/reset`)
 })
+
+Cypress.Commands.add('addUser', (userInfo) => {
+  cy.request('POST', `${baseUrl}/users`, userInfo)
+})
